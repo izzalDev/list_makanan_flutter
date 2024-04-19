b08b544dd2638be74fc3e03343a34d92671fbaea
Rizal Fadlullah <rizal.fadlullah@gmail.com>
2024-04-19 11:15:58 +0700

    sudah selesai

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:list_makanan_flutter/list_item.dart';
+import 'package:list_makanan_flutter/styles.dart';
+import 'package:list_makanan_flutter/makanan.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Icon(Icons.list_alt_sharp, size: 30),
+          SizedBox(width: 10),
+          Text('List Kuliner', style: textHeader1),
+        ]),
+        Expanded(
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: ListView.builder(
+                itemCount: listMakanan.length,
+                itemBuilder: (context, index) {
+                  return ListItem(
+                    nama: listMakanan[index].nama,
+                    harga: listMakanan[index].harga,
+                    gambarlain: listMakanan[index].gambarlain,
+                    detail: listMakanan[index].detail,
+                    kalori: listMakanan[index].kalori,
+                    bahan: listMakanan[index].bahan,
+                    waktubuka: listMakanan[index].waktubuka,
+                    deskripsi: listMakanan[index].deskripsi,
+                    gambar: listMakanan[index].gambar,
+                  );
+                },
+              )),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/list_item.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:list_makanan_flutter/detail_page.dart';
+
+class ListItem extends StatelessWidget {
+  final String nama;
+  final String detail;
+  final String deskripsi;
+  final String gambar;
+  final String waktubuka;
+  final String harga;
+  final String kalori;
+  final List<String> gambarlain;
+  final List<Map<String, String>> bahan;
+
+  const ListItem(
+      {super.key,
+      required this.nama,
+      required this.harga,
+      required this.gambarlain,
+      required this.detail,
+      required this.kalori,
+      required this.bahan,
+      required this.waktubuka,
+      required this.deskripsi,
+      required this.gambar});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 178, 178, 178),
+              offset: Offset(1.0, 2.0),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 85,
+              height: 75,
+              color: Colors.grey,
+              child: Image.asset(
+                gambar,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  nama,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  deskripsi,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(
+                nama: nama,
+                harga: harga,
+                gambarlain: gambarlain,
+                detail: detail,
+                kalori: kalori,
+                waktubuka: waktubuka,
+                deskripsi: deskripsi,
+                gambar: gambar,
+                bahan: bahan),
+          ),
+        );
+      },
+    );
+  }
+}
